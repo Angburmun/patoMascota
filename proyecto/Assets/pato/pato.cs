98c16b9853156246d3c61ba15a236ca4369747fb
Álvaro Donderis <alvarodonderis@gmail.com>
2022-10-28 17:15:41 +0200

    Auto stash before merge of "main" and "origin/main"

--- proyecto/Assets/pato/pato.cs
+++ proyecto/Assets/pato/pato.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class pato : MonoBehaviour
 {
     public double hambre;
+    public double cansancio;
+    public double aburrimiento;
+    public double sufrimiento;
+    float x;
 
     // Start is called before the first frame update
     void Start()
     {
         hambre = 100;
+        cansancio = 100;
+        aburrimiento = 100;
+        sufrimiento = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hambre > 0) {hambre -= 0.3;}
+        x += Time.deltaTime;
+
+        if (x > 3) { 
+        if (hambre > 0) {hambre -= sufrimiento;}
             
         print(hambre);
-    }
+
+        if (cansancio > 0) { cansancio -= sufrimiento;}
+
+        print(cansancio);
+
+        if (aburrimiento > 0) { aburrimiento -= sufrimiento; }
+
+        print(aburrimiento);
+            x = 0;
+        }
+       }
 }
